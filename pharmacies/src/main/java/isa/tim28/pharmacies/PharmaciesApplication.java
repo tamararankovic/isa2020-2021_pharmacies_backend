967693f35f2c21ae8360cfdd643d47f1a3344198
= <rankovictamaraa@gmail.com>
2021-01-27 15:51:31 +0100

    projekat inicijalizovan

---
+++ pharmacies/src/main/java/isa/tim28/pharmacies/PharmaciesApplication.java
@@ -0,0 +1,13 @@
+package isa.tim28.pharmacies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaciesApplication.class, args);
+	}
+
+}
---
+++ pharmacies/src/test/java/isa/tim28/pharmacies/PharmaciesApplicationTests.java
@@ -0,0 +1,13 @@
+package isa.tim28.pharmacies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
