c858ff3c2bad10a94b1a61241459c770d144cfde
= <rankovictamaraa@gmail.com>
2021-01-31 22:09:08 +0100

    testni podaci za apoteku i farmaceuta

--- pharmacies/src/main/resources/data-postgres.sql
+++ pharmacies/src/main/resources/data-postgres.sql
@@ -4,3 +4,9 @@ insert into users(id, name, surname, email, password, role) values (3, 'Tamara',
 insert into users(id, name, surname, email, password, role) values (4, 'Tamara', 'Rankovic', 'email4@gmail.com', '1234', 3);
 insert into users(id, name, surname, email, password, role) values (5, 'Tamara', 'Rankovic', 'email5@gmail.com', '1234', 4);
 insert into users(id, name, surname, email, password, role) values (6, 'Tamara', 'Rankovic', 'email6@gmail.com', '1234', 5);
+
+insert into pharmacy(id, name, description, address) values (1, 'Pharmacy 1', 'Some description of pharmacy 1', 'Masarikova 1, Novi Sad');
+
+insert into engagement_in_pharmacy(id, pharmacy_id) values (1, 1);
+
+insert into pharmacist(id, engegement_in_pharmacy_id, user_id) values (1, 1, 2);
