8da49093cfea5068a7adc06ea97fd74a20e7166c
= <rankovictamaraa@gmail.com>
2021-02-08 16:00:00 +0100

    spremanje projekta za deployment

--- pharmacies/src/main/java/isa/tim28/pharmacies/config/WebConfig.java
+++ pharmacies/src/main/java/isa/tim28/pharmacies/config/WebConfig.java
@@ -1,6 +1,8 @@
 package isa.tim28.pharmacies.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
  
+	@Autowired
+	private Environment env;
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-    	registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowCredentials(true);
+    	registry.addMapping("/**").allowedOrigins(env.getProperty("front.url")).allowCredentials(true);
     }
 }
