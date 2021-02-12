d452555fed54a4b3456c98efc3603ca36d07508a
= <rankovictamaraa@gmail.com>
2021-02-12 18:50:54 +0100

    pokusaj podesavanja konfiguracije za travis ci

--- pharmacies/src/test/java/isa/tim28/pharmacies/PharmaciesApplicationTests.java
+++ pharmacies/src/test/java/isa/tim28/pharmacies/PharmaciesApplicationTests.java
@@ -2,10 +2,12 @@ package isa.tim28.pharmacies;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles({ "test" })
 class PharmaciesApplicationTests {
 
 }
